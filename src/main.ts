d7e7212282b886cd53c03473046fa658ab5e5483
Anna Surahatava <anika3160@gmail.com>
2025-11-30 21:29:42 +0100

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-import 'dotenv/config';
-import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
+import { readFileSync } from 'fs';
+import { load as loadYaml } from 'js-yaml';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +16,15 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  SwaggerModule.setup(
+    'doc',
+    app,
+    loadYaml(
+      readFileSync(join(__dirname, '..', 'doc', 'api.yaml'), 'utf-8'),
+    ) as OpenAPIObject,
+  );
+
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
